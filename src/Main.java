c8fb4fdc762bcd49105c1879f635595d9af14d57
陈·风暴烈酒 <1055353746@qq.com>
2019-06-25 16:55:57 +0800

    测试Url

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     //自己实现一个简单的tomcat服务器 能够正常接收请求,处理业务逻辑并做出响应
     public static void main(String[] args) {
         //启动我们的TomPig服务器,然后就可以在浏览器里访问这个猪册项目啦
-        //测试url:
+        //测试url: localhost:8080/user/login?username=pan_pan&password=66666
         TomPig tomPig = new TomPig();
         tomPig.start();
     }
